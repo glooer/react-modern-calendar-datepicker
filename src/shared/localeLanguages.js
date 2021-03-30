6c00be00a5c5a31cfeb9f5b0e3d219dc2bd9617c
Kostya Polenkov <polenkov-kp@ural.rt.ru>
2021-03-30 17:14:35 +0500

    добавление русской локализации

--- src/shared/localeLanguages.js
+++ src/shared/localeLanguages.js
@@ -74,6 +74,103 @@ const localeLanguages = {
     yearLetterSkip: -2,
     isRtl: true,
   },
+
+  ru: {
+    // months list by order
+    months: [
+      'Январь',
+      'Февраль',
+      'Март',
+      'Апрель',
+      'Май',
+      'Июнь',
+      'Июль',
+      'Август',
+      'Сентябрь',
+      'Октябрь',
+      'Ноябрь',
+      'Декабрь',
+    ],
+
+    // week days by order
+    weekDays: [
+      {
+        name: 'Понедельник',
+        short: 'ПН',
+      },
+      {
+        name: 'Вторник',
+        short: 'ВТ',
+      },
+      {
+        name: 'Среда',
+        short: 'СР',
+      },
+      {
+        name: 'Четверг',
+        short: 'ЧТ',
+      },
+      {
+        name: 'Пятница',
+        short: 'ПТ',
+      },
+      {
+        name: 'Суббота',
+        short: 'СБ',
+        isWeekend: true,
+      },
+      {
+        name: 'Воскресенье', // used for accessibility
+        short: 'ВС', // displayed at the top of days' rows
+        isWeekend: true, // is it a formal weekend or not?
+      },
+    ],
+
+    // just play around with this number between 0 and 6
+    weekStartingIndex: 6,
+
+    // return a { year: number, month: number, day: number } object
+    getToday(gregorainTodayObject) {
+      return gregorainTodayObject;
+    },
+
+    // return a native JavaScript date here
+    toNativeDate(date) {
+      return new Date(date.year, date.month - 1, date.day);
+    },
+
+    // return a number for date's month length
+    getMonthLength(date) {
+      return new Date(date.year, date.month, 0).getDate();
+    },
+
+    // return a transformed digit to your locale
+    transformDigit(digit) {
+      return digit;
+    },
+
+    // texts in the date picker
+    nextMonth: 'Следующий месяц',
+    previousMonth: 'Предыдущий месяц',
+    openMonthSelector: 'Открыть выбор месяца',
+    openYearSelector: 'Открыть выбор года',
+    closeMonthSelector: 'Закрыть выбор месяца',
+    closeYearSelector: 'Закрыть выбор года',
+    defaultPlaceholder: '',
+
+    // for input range value
+    from: 'from',
+    to: 'to',
+
+    // used for input value when multi dates are selected
+    digitSeparator: '.',
+
+    // if your provide -2 for example, year will be 2 digited
+    yearLetterSkip: 0,
+
+    // is your language rtl or ltr?
+    isRtl: false,
+  }
 };
 
 const getLocaleDetails = locale => {
